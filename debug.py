8435e2dc7168110c2657fe597af65e982c106a79
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-02-29 10:39:36 +0100

    debug practice/general trial functionality

--- debug.py
+++ debug.py
@@ -5,6 +5,14 @@ of the 'unpredictable flickering null-cue experiment'.
 made by Anna van Harmelen, 2024
 """
 
+from set_up import get_monitor_and_dir, get_settings
+from practice import practice
+
+monitor, directory = get_monitor_and_dir(True)
+settings = get_settings(monitor, directory)
+
+practice(True, settings)
+
 # stop here
 import sys
 
