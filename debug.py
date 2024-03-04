d3957dd8dfb158e39fe786a772b3c266cd4a07b8
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-03-04 14:12:10 +0100

    debug single trials

--- debug.py
+++ debug.py
@@ -7,11 +7,20 @@ made by Anna van Harmelen, 2024
 
 from set_up import get_monitor_and_dir, get_settings
 from practice import practice
+from trial import generate_stimuli_characteristics, single_trial
+
 
 monitor, directory = get_monitor_and_dir(True)
 settings = get_settings(monitor, directory)
 
-practice(True, settings)
+stimuli_characteristics: dict = generate_stimuli_characteristics("congruent", "left", "invisible", "middle")
+
+report: dict = single_trial(
+    **stimuli_characteristics,
+    settings=settings,
+    testing=True,
+    eyetracker=None,
+)
 
 # stop here
 import sys
