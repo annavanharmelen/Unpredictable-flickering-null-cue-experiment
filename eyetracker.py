d8f4f23810175c1b856147284eb0090ec163b6e7
Anna work laptop <a.m.van.harmelen@vu.nl>
2024-03-13 15:41:18 +0100

    update trigger codes

--- eyetracker.py
+++ eyetracker.py
@@ -48,17 +48,25 @@ class Eyelinker:
         self.tracker.close_edf()
 
 
-def get_trigger(frame, condition, target_position):
-    condition_marker = {"congruent": 1, "incongruent": 3, "neutral": 5}[condition]
+def get_trigger(frame, predictability, timing, congruency, flicker_type, location):
+    condition_marker = {"predictable": 1, "unpredictable": 37}[predictability]
+    
+    condition_marker += {"early": 0, "middle": 12, "late": 24}[timing]
 
-    if target_position == "right":
+    if congruency == "incongruent":
+        condition_marker += 6
+
+    condition_marker += {"stable": 0, "high_freq": 2, "low_freq": 4}[flicker_type]
+
+    if location == "right":
         condition_marker += 1
 
     return {
-        "stimuli_onset": "",
-        "capture_cue_onset": "1",
-        "probe_cue_onset": "2",
-        "response_onset": "3",
-        "response_offset": "4",
-        "feedback_onset": "5",
+        "just_code_please": "",
+        "stimuli_onset": "1",
+        "capture_cue_onset": "2",
+        "probe_cue_onset": "3",
+        "response_onset": "4",
+        "response_offset": "5",
+        "feedback_onset": "6",
     }[frame] + str(condition_marker)
